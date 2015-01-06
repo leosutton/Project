c9c0a5b094f355f734e3ea0c8d1349643d5179c2
Leo Sutton <leo.sutton@talktalk.net>
2015-01-06 12:02:43 +0000

    Tkinter

--- MainController.py
+++ MainController.py
@@ -2,17 +2,22 @@ from Graph import Graph
 from GraphDrawrer import GraphDrawrer
 from Environment import Environment
 import pygame
+from MenuScreen import Menu
 
-graph = Graph()
-environment = Environment(graph)
-graphDrawrer = GraphDrawrer(graph)
+menu = Menu()
 
-pygame.init()
-surface = pygame.display.set_mode((1600, 600))
+#
+# graph = Graph()
+# environment = Environment(graph)
+# graphDrawrer = GraphDrawrer(graph)
+#
+# pygame.init()
+# surface = pygame.display.set_mode((1600, 600))
+#
+# while True:
+#     surface.blit(environment.make_frame(), (0,0))
+#     surface.blit(graphDrawrer.make_frame(), (800,0))
+#     pygame.display.update()
+#     clock = pygame.time.Clock()
+#     clock.tick(60)
 
-while True:
-    surface.blit(environment.make_frame(), (0,0))
-    surface.blit(graphDrawrer.make_frame(), (800,0))
-    pygame.display.update()
-    clock = pygame.time.Clock()
-    clock.tick(60)
